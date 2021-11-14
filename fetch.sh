d929aa4fd34ce319c520e33dfe8bd6e5b4664d20
sbohmann <mail@sebastian-bohmann.com>
2021-11-14 08:32:30 +0100

    generate.sh: copying libraries from node_modules

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 echo Fetching data from data.gv.at
 cd raw_data
-wget -nc https://info.gesundheitsministerium.gv.at/data/timeline-faelle-ems.csv
-wget -nc https://covid19-dashboard.ages.at/data/Hospitalisierung.csv
+wget -O timeline-faelle-ems.csv https://info.gesundheitsministerium.gv.at/data/timeline-faelle-ems.csv
+wget -O Hospitalisierung.csv https://covid19-dashboard.ages.at/data/Hospitalisierung.csv
