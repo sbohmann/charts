d528cda9faf124f98af93a5e3cdc37aaeb8bba1d
sbohmann <mail@sebastian-bohmann.com>
2021-11-17 12:22:00 +0100

    Moved templates out of raw_data

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 echo Fetching data from data.gv.at
+if [[ ! -e raw_data ]]; then mkdir raw_data; fi
 cd raw_data
 wget -O timeline-faelle-ems.csv https://info.gesundheitsministerium.gv.at/data/timeline-faelle-ems.csv
 wget -O Hospitalisierung.csv https://covid19-dashboard.ages.at/data/Hospitalisierung.csv
--- generate.sh
+++ generate.sh
@@ -2,7 +2,6 @@
 export generate_js_location="$1"
 ./fetch.sh
 pwd
-if [[ ! -e raw_data ]]; then mkdir raw_data; fi
 if [[ ! -e static/libraries ]]; then mkdir static/libraries; fi
 if [[ ! -e static/libraries/js-joda.js ]]; then cp node_modules/@js-joda/core/dist/js-joda.js static/libraries; fi
 node raw_data_conversion/dataGvAt.js
