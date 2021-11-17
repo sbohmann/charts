4de2d1fc21d7542f38ae16a7891b347ad6680d56
sbohmann <mail@sebastian-bohmann.com>
2021-11-17 17:04:51 +0100

    Always copying js-joda.js*

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@ export generate_js_location="$1"
 ./fetch.sh
 pwd
 if [[ ! -e static/libraries ]]; then mkdir static/libraries; fi
-if [[ ! -e static/libraries/js-joda.js ]]; then cp node_modules/@js-joda/core/dist/js-joda.js static/libraries; fi
+cp node_modules/@js-joda/core/dist/js-joda.js* static/libraries
 node raw_data_conversion/dataGvAt.js
 echo running "[$generate_js_location]"
 node "$generate_js_location"
