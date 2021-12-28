42caa5d04e4cb2e48686eb23d1cae6afb42b90c7
sbohmann <mail@sebastian-bohmann.com>
2021-12-28 08:35:25 +0100

    Last date in data.js

--- raw_data_conversion/output.js
+++ raw_data_conversion/output.js
@@ -36,7 +36,8 @@ module.exports.forDatasets = function (datasetNames, dataRows) {
         for (const name of datasetNames) {
             let values = []
             let previousValue = null
-            for (let date = minimumDate; date.compareTo(maximumDate) <= 0; date = date.plusDays(1)) {
+            let lastDate = minimumDate
+            for (let date = minimumDate; date.compareTo(lastDate) <= 0; date = date.plusDays(1)) {
                 let value = data.get(date.toString()).get(name)
                 if (value != null) {
                     values.push(value)
@@ -44,10 +45,12 @@ module.exports.forDatasets = function (datasetNames, dataRows) {
                     values.push(0)
                 }
                 previousValue = value
+                lastDate = date
             }
             output.push({
                 name: name,
                 firstDate: minimumDate,
+                lastDate: lastDate,
                 points: values
             })
         }
